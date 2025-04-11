91b980eb9f4ff1584e349bf5f7557805086b043f
UmeshReddy <umeshreddy4697@gmail.com>
2025-04-11 19:41:57 +0530

    initial commit

---
+++ src/main/java/com/authentication/authentication_app/AuthenticationAppApplication.java
@@ -0,0 +1,13 @@
+package com.authentication.authentication_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationAppApplication.class, args);
+	}
+
+}
